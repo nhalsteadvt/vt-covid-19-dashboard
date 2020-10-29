22a5c32f69028781b217281911bd6f5d471e6d05
nhalsteadvt <73657669+nhalsteadvt@users.noreply.github.com>
2020-10-29 15:24:16 -0400

    Added comments
    Added comments for methods and certain blocks of code.

--- src/DataParser.java
+++ src/DataParser.java
@@ -10,16 +10,21 @@ import java.util.ArrayList;
 
 /**
  * @author nhalstead
+ *         The DataParser class deals with parsing the given file dashboard.txt
+ *         from the Data folder to create Entry objects and place them into an
+ *         ArrayList organized from least to most recent
  *
  */
 public class DataParser {
 
-    private static ArrayList<Entry> dataCollection;
+    private static ArrayList<Entry> dataCollection; // this contains all of the
+                                                    // Entry objects
 
     public static void main(String[] args) throws IOException {
         File dash = null;
         dataCollection = new ArrayList<>();
 
+        // this block ensures the file exists where it is expected
         try {
             dash = new File("Data\\dashboard.txt");
             if (!dash.exists()) {
@@ -30,7 +35,10 @@ public class DataParser {
             System.out.println(err.getMessage());
         }
 
+        // call to method to add Entry objects to the dataCollection object
         parseFile(dash);
+
+        // this block is for debugging purposes
         for (int i = 0; i < dataCollection.size(); i++) {
             System.out.println(dataCollection.get(i).toString());
         }
@@ -38,6 +46,18 @@ public class DataParser {
     }
 
 
+    /**
+     * This method will parse a file and add entries to the dataCollection
+     * object based on contents of the file
+     * 
+     * Precondition: The file is of correct format (which it will be unless
+     * tampered with)
+     * 
+     * Postcondition: The file is left unaltered; there are no data leaks
+     * 
+     * @param dash
+     * @throws IOException
+     */
     public static void parseFile(File dash) throws IOException {
         RandomAccessFile dashReader = new RandomAccessFile(dash, "r");
         int numLines = lineCounter(dash);
@@ -45,13 +65,13 @@ public class DataParser {
         int currLineNum = 1;
         int lastByte = 0;
 
+        // This block parses the rest of the file with the data
         while (lastByte != -1) {
             lastByte = dashReader.read();
             String line = dashReader.readLine();
             if (line == null) {
                 break;
             }
-            // System.out.println(line);
             currLineNum++;
             int day = numLines - currLineNum;
             String noDate = line.substring(line.indexOf("\t") + 1);
@@ -61,29 +81,37 @@ public class DataParser {
             int positive = Integer.parseInt((noTests.substring(0, noTests
                 .indexOf("\t"))).replace(",", ""));
             Entry temp = new Entry(day, tests, positive);
-            // System.out.println("temp: " + temp.toString());
             dataCollection.add(0, temp);
-
         }
 
         dashReader.close();
-
     }
 
 
+    /**
+     * This method counts how many lines are in the file
+     * 
+     * @param file
+     *            The input file
+     * @return int for how many visible lines there are in the file
+     * @throws IOException
+     */
     public static int lineCounter(File file) throws IOException {
         RandomAccessFile fileReader = new RandomAccessFile(file, "r");
         int numLines = 0;
         int lastByte = 0;
 
+        // This block parses the file and increments numLines for each line
         while (lastByte != -1) {
             lastByte = fileReader.read();
-            fileReader.readLine();
+            if (fileReader.readLine() == null) {
+                break;
+            }
             numLines++;
         }
 
         fileReader.close();
-
+        System.out.println(numLines);
         return numLines;
     }
 
--- src/Entry.java
+++ src/Entry.java
@@ -4,13 +4,23 @@
 
 /**
  * @author nhalstead
- *
+ *         The Entry class is used for storing a single point of data (one day)
  */
 public class Entry {
     private int day;
     private int tests;
     private int positive;
 
+    /**
+     * 
+     * @param day
+     *            how many days since the earliest entry
+     *            (starting at 0)
+     * @param tests
+     *            how many tests were performed that day
+     * @param positive
+     *            how many of those tests were positive for Covid-19
+     */
     public Entry(int day, int tests, int positive) {
         this.day = day;
         this.tests = tests;
@@ -18,21 +28,42 @@ public class Entry {
     }
 
 
+    /**
+     * Gets the day of Entry
+     * 
+     * @return int day
+     */
     public int getDay() {
         return day;
     }
 
 
+    /**
+     * Gets the tests of Entry
+     * 
+     * @return int tests
+     */
     public int getTests() {
         return tests;
     }
 
 
+    /**
+     * Gets the positive of Entry
+     * 
+     * @return int positive
+     */
     public int getPositive() {
         return positive;
     }
 
 
+    /**
+     * This method calculates the percentage positive out of the total tests.
+     * If the denominator (tests performed) is zero, zero is returned.
+     * 
+     * @return double representing the percentage positive
+     */
     public double calcPercentage() {
         if (tests == 0) {
             return 0;
@@ -41,12 +72,15 @@ public class Entry {
     }
 
 
+    /**
+     * Overrides the default toString() method to represent all stored data
+     * Includes the percentage positive to 4 decimal points
+     */
+    @Override
     public String toString() {
         String ans = String.format("%d: %d/%d = %.4f%s", day, positive, tests,
             100 * calcPercentage(), "%");
         return ans;
-        // "" + day + ": " + positive + "/" + tests + " = "
-        // + calcPercentage();
     }
 
 }
