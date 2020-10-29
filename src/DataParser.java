e9af61664796baa1c2acf0d232b2d9ba52c5e3d5
nhalsteadvt <73657669+nhalsteadvt@users.noreply.github.com>
2020-10-29 12:56:04 -0400

    Added line counter
    added a method to count number of lines (useful since data is organized by most recent).
    also closed random access file objects to prevent leaks

--- src/DataParser.java
+++ src/DataParser.java
@@ -37,7 +37,27 @@ public class DataParser {
 
     public static void parseFile(File dash) throws IOException {
         RandomAccessFile dashReader = new RandomAccessFile(dash, "r");
+        int numLines = lineCounter(dash);
 
+        dashReader.close();
+
+    }
+
+
+    public static int lineCounter(File file) throws IOException {
+        RandomAccessFile fileReader = new RandomAccessFile(file, "r");
+        int numLines = 0;
+        int lastByte = 0;
+
+        while (lastByte != -1) {
+            lastByte = fileReader.read();
+            fileReader.readLine();
+            numLines++;
+        }
+
+        fileReader.close();
+
+        return numLines - 1;
     }
 
 }
